f1a4a9f794b129937f8d6fa803842310dc0bbac7
markkauffman22 <55072954+markkauffman22@users.noreply.github.com>
2019-12-23 07:14:59 -0700

    Initial commit. Hello World with React.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Inject HTML into website with React!
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+// React works by creating JSX files. The HTML above is picked up by a
+// compiler and it gets converted or compiled down to JavaScript.
+// The compiler comes for the require("react") line above.
+
+//
